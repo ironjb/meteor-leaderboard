f8eaa9793c63c67f1cda3434792553c231315073
Joe Bell <joe.bell@mdu.com>
2015-03-09 17:24:27 -0600

    5. Templates

--- meteor_leaderboard.js
+++ meteor_leaderboard.js
@@ -1,1 +1,12 @@
 PlayersList = new Mongo.Collection('players');
+
+if (Meteor.isClient) {
+	Template.leaderboard.helpers({
+		'player': function () {
+			return PlayersList.find();
+		}
+	});
+}
+
+if (Meteor.isServer) {
+}
