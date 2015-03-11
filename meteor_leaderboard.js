d532ae2ec4308e315f6c75e6203db24aeab3e769
Joe Bell <joe.bell@mdu.com>
2015-03-11 15:55:45 -0600

    8. Databases, Part 2

--- meteor_leaderboard.js
+++ meteor_leaderboard.js
@@ -3,7 +3,7 @@ PlayersList = new Mongo.Collection('players');
 if (Meteor.isClient) {
 	Template.leaderboard.helpers({
 		player: function () {
-			return PlayersList.find();
+			return PlayersList.find({}, { sort: { score: -1, name: 1 } });
 		},
 		selectedClass: function () {
 			var playerId = this._id;
@@ -11,6 +11,10 @@ if (Meteor.isClient) {
 			if (playerId === selectedPlayer) {
 				return 'selected';
 			}
+		},
+		showSelectedPlayer: function () {
+			var selectedPlayer = Session.get('selectedPlayer');
+			return PlayersList.findOne(selectedPlayer);
 		}
 	});
 
@@ -18,6 +22,14 @@ if (Meteor.isClient) {
 		'click .player': function() {
 			var playerId = this._id;
 			Session.set('selectedPlayer', playerId);
+		},
+		'click .increment': function () {
+			var selectedPlayer = Session.get('selectedPlayer');
+			PlayersList.update(selectedPlayer, { $inc: { score: 5 } });
+		},
+		'click .decrement': function () {
+			var selectedPlayer = Session.get('selectedPlayer');
+			PlayersList.update(selectedPlayer, { $inc: { score: -5 } });
 		}
 	});
 }
